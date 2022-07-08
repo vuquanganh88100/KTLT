47839ab921d57acedd3e053e7f81b7d6602d4c65
vuquanganh88100 <74352037+vuquanganh88100@users.noreply.github.com>
2022-07-08 14:40:27 +0700

    Update Model.cpp

--- VQA/Model.cpp
+++ VQA/Model.cpp
@@ -1,15 +1,25 @@
 #include "Student.h"
 #include<iostream>
 using namespace std;
-Model_program::Model_program() :Student() {
-	gpa = 0;
-}
-int Model_program::getGpa() {
-	return Model_program::getGpa();
-}
-void Model_program::setGpa() {
-	this->gpa = gpa;
-}
-void Model_program::display() {
-	cout << "Gpa: " << gpa << endl;
-}
+
+
+Model_program::Model_program(string name, int id, int course, int check, double gpa) : Student(name, id, course, check) {
+		this->gpa = gpa;
+	}
+
+	int Model_program::getGpa() {
+		return gpa;
+	}
+	void Model_program::setGpa() {
+		this->gpa = gpa;
+	}
+	void Model_program::display() {
+		Student::display();
+		cout << "Gpa: " << gpa << endl;
+		if (gpa < 5.0) {
+			cout << "GPA <5.0 so not complete";
+		}
+		else {
+			cout << "Congratulations on your graduation";
+		}
+	}
