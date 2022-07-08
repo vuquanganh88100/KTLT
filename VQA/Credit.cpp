57234a1a7aa1509e2bd36ef280a71aefef044260
vuquanganh88100 <74352037+vuquanganh88100@users.noreply.github.com>
2022-07-08 14:40:14 +0700

    Update Credit.cpp

--- VQA/Credit.cpp
+++ VQA/Credit.cpp
@@ -1,16 +1,26 @@
 #include "Student.h"
 #include<iostream>
+
 using namespace std;
-Credit_program::Credit_program() :Student(){
-	number_of_credit = 0;
-} 
-int Credit_program::getNumber_of_credit() {
-	return Credit_program::getNumber_of_credit();
-}
-void Credit_program::setNumber_of_credit() {
-	this->number_of_credit = number_of_credit;
-}
-void Credit_program::display() {
-	Student::display();
-	cout << "The number of credit: " << number_of_credit << endl;
-}
+
+
+	Credit_program::Credit_program(string name, int id, int course, int check, int number_of_credit) : Student(name, id, course, check) {
+		this->number_of_credit = number_of_credit;
+	}
+
+	int Credit_program::getNumber_of_credit() {
+		return number_of_credit;
+	}
+	void Credit_program::setNumber_of_credit() {
+		this->number_of_credit = number_of_credit;
+	}
+	void Credit_program::display() {
+		Student::display();
+		cout << "The number of credit:" << number_of_credit << endl;
+		if (number_of_credit < 128) {
+			cout << "The number of credit <128 so not complete";
+		}
+		else {
+			cout << "Congratulations on your graduation";
+		}
+	}
