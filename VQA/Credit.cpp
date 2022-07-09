3f32f8aed7c92cdfc0fdff05bd6d84afc382c318
vuquanganh88100 <74352037+vuquanganh88100@users.noreply.github.com>
2022-07-09 20:53:45 +0700

    Update Credit.cpp

--- VQA/Credit.cpp
+++ VQA/Credit.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 	Credit_program::Credit_program(string name, int id, int course, int check, int number_of_credit) : Student(name, id, course, check) {
 		this->number_of_credit = number_of_credit;
 	}
@@ -14,6 +13,7 @@ using namespace std;
 	void Credit_program::setNumber_of_credit() {
 		this->number_of_credit = number_of_credit;
 	}
+
 	void Credit_program::display() {
 		Student::display();
 		cout << "The number of credit:" << number_of_credit << endl;
