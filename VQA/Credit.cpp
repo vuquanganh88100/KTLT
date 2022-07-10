63c3a4e01681a953df377d5d8a1d52b7aa89fe07
vuquanganh88100 <74352037+vuquanganh88100@users.noreply.github.com>
2022-07-10 17:29:02 +0700

    Update Credit.cpp

--- VQA/Credit.cpp
+++ VQA/Credit.cpp
@@ -3,24 +3,25 @@
 
 using namespace std;
 
-	Credit_program::Credit_program(string name, int id, int course, int check, int number_of_credit) : Student(name, id, course, check) {
-		this->number_of_credit = number_of_credit;
-	}
+Credit_program::Credit_program(string name, int id, int course, int check, int number_of_credit) : Student(name, id, course, check) {
+	this->number_of_credit = number_of_credit;
+}
 
-	int Credit_program::getNumber_of_credit() {
-		return number_of_credit;
-	}
-	void Credit_program::setNumber_of_credit() {
-		this->number_of_credit = number_of_credit;
-	}
+int Credit_program::getNumber_of_credit() {
+	return number_of_credit;
+}
+void Credit_program::setNumber_of_credit(int credit) {
+	this->number_of_credit = credit;
+}
 
-	void Credit_program::display() {
-		Student::display();
-		cout << "The number of credit:" << number_of_credit << endl;
-		if (number_of_credit < 128) {
-			cout << "The number of credit <128 so not complete";
-		}
-		else {
-			cout << "Congratulations on your graduation";
-		}
+void Credit_program::display() {
+	Student::display();
+	cout << "The number of credit:" << number_of_credit << endl;
+	if (number_of_credit < 128) {
+		cout << "The number of credit <128 so not complete";
 	}
+	else {
+		cout << "Congratulations on your graduation";
+	}
+	cout << "\n====================================================================" << endl;
+}
